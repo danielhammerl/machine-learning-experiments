75615d0cb40b9cf20fd58cd736def7bf154fd927
danielhammerl <daniel.hammerl99@gmx.de>
2023-05-24 11:32:32 +0200

    stuff

--- src/framework/engine/world/World.cpp
+++ src/framework/engine/world/World.cpp
@@ -188,9 +188,9 @@ unsigned int World::getNumberOfPopulation() {
 }
 
 void World::populateByGenomes(std::vector<std::string> genomes, unsigned int population, float mutationRate) {
-    int genomesLength = std::max((int) genomes.size() * 2, (int) population);
+    int genomesLength = std::min((int) genomes.size() * 2, (int) population);
     for (auto x = 0; x < genomesLength; x++) {
-        auto genome = Genome(genomes[x]);
+        auto genome = Genome(genomes[x % genomesLength]);
         genome.mutateGenome(mutationRate);
         bool success = false;
         while (!success) {
